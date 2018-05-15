7920d8d2161d2a8d525a095239ebcc263a13a957
Arul_Eb3 <arulguru6@gmail.com>
2018-05-15 12:22:00 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { EditComponent } from './components/edit/edit.component';
+import { IndexComponent } from './components/index/index.component';
+import { CreateComponent } from './components/create/create.component';
+import { appRoutes } from './routerConfig';
+import { CoinService } from './service/coin.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditComponent,
+    IndexComponent,
+    CreateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
+    
   ],
-  providers: [],
+  providers: [CoinService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
